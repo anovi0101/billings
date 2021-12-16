a6db8dda28e7ae095c7bfeaabfbc6fc8a3db9558
anovi0101 <60299542+anovi0101@users.noreply.github.com>
2021-12-16 17:41:47 +0200

    Get invoices from built Web service

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,8 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "../styles/globals.css";
+import axios from "axios";
 const theme = createTheme();
-
-const invoiceNumbers = [
-  { id: 8, status: "ISSUED", due: "21/11/2020", amount: 79.99 },
-  { id: 7, status: "OVERDUE", due: "21/10/2020", amount: 49.99 },
-  { id: 6, status: "OVERDUE", due: "21/09/2020", amount: 79.99 },
-  { id: 5, status: "PAID", due: "21/08/2020", amount: 49.99 },
-  { id: 4, status: "PAID", due: "21/07/2020", amount: 29.99 },
-  { id: 3, status: "PAID", due: "21/06/2020", amount: 79.99 },
-  { id: 2, status: "PAID", due: "21/05/2020", amount: 39.99 },
-  { id: 1, status: "PAID", due: "21/04/2020", amount: 79.99 },
-];
 
 function BillingApp({ Component, pageProps }) {
   React.useEffect(() => {
@@ -26,7 +16,18 @@ function BillingApp({ Component, pageProps }) {
     }
   }, []);
 
-  const [state, setState] = React.useState(invoiceNumbers);
+  const [state, setState] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`http://${process.env.NEXT_PUBLIC_API_URL}/api/invoices`)
+      .then((response) => {
+        setState(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
